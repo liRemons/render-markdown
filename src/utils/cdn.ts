235b115a9363b474bdc93e3174d6ed4df5787473
liRemons <remons@foxmail.com>
2026-08-06 14:32:02 +0800

    feat: update version

--- src/utils/cdn.ts
+++ src/utils/cdn.ts
@@ -1,5 +1,5 @@
 const js = [
-  { externalsName: 'mermaid', url: "https://registry.npmmirror.com/mermaid/11.16.0/files/dist/mermaid.min.js" },
+  { externalsName: 'mermaid', url: "https://registry.npmmirror.com/mermaid/11.16.1/files/dist/mermaid.min.js" },
 ];
 
 export default js;
